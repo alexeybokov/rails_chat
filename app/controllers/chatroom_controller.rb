f1f73453f2e3c333816528f50ee0d94e73958e28
Alexeybokov <lexabokov18@gmail.com>
2020-05-22 18:22:38 +0300

    update chatroom controller

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,7 @@
 class ChatroomController < ApplicationController
   def index
+    @online_users = User.where(status: 'Online')
+    @users = User.all
     @message = Message.new
     @recently_messages = Message.recently_ten_messages
   end
